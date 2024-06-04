304bd005eca515734311010d22a77987bc33be93
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-04 17:37:13 +0700

    Randomize cells every second

--- src/Cells.jsx
+++ src/Cells.jsx
@@ -1,11 +1,17 @@
 import { createCells } from './golLogic'
-import { Graphics } from '@pixi/react'
-import { useCallback } from 'react'
+import { Graphics, useTick } from '@pixi/react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Cells({ windowSize }) {
+  const [cells, setCells] = useState(createCells(windowSize))
+  const cellsGraphics = []
+
+  useEffect(() => {
+    setCells(createCells(windowSize))
+  }, [windowSize])
+
   const cellGraphics = useCallback((g, cell) => {
     const cellColor = cell.isAlive ? 0x000000 : 0xffffff
-
     g.clear()
     g.beginFill(cellColor)
     g.lineStyle(1, 0x000000)
@@ -13,15 +19,22 @@ export default function Cells({ windowSize }) {
     g.endFill()
   }, [])
 
-  const cells = createCells(windowSize)
-  const cellsGraphics = []
-  cells.map((row) => {
-    row.map((cell) => {
+  cells.forEach((row) => {
+    row.forEach((cell) => {
       const graphics = (
         <Graphics key={cell.key} draw={(g) => cellGraphics(g, cell)} />
       )
       cellsGraphics.push(graphics)
     })
+  })
+
+  let stopwatch = 0
+  useTick((delta) => {
+    stopwatch += delta
+    if (stopwatch > 60) {
+      stopwatch = 0
+      setCells(createCells(windowSize))
+    }
   })
 
   return cellsGraphics
