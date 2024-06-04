1120f0939065d15d21d44f4bf231d32b170a71da
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-04 10:30:07 +0700

    Install pixijs

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import { TextStyle } from 'pixi.js'
+import { Stage, Container, Sprite, Text } from '@pixi/react'
+
 export default function App() {
-  return <h1>Hello, world!</h1>
+  const bunnyUrl = 'https://pixijs.io/pixi-react/img/bunny.png'
+  return (
+    <Stage x={800} y={600} options={{ background: 0x1099bb }}>
+      <Sprite image={bunnyUrl} x={300} y={150} />
+      <Sprite image={bunnyUrl} x={500} y={150} />
+      <Sprite image={bunnyUrl} x={400} y={200} />
+
+      <Container x={200} y={200}>
+        <Text
+          text="Hello World"
+          anchor={0.5}
+          x={220}
+          y={150}
+          filters={[]}
+          style={
+            new TextStyle({
+              align: 'center',
+              fontSize: 50,
+              letterSpacing: 20,
+              dropShadow: true,
+              dropShadowColor: 0xffffff,
+              dropShadowDistance: 6
+            })
+          }
+        />
+      </Container>
+    </Stage>
+  )
 }
